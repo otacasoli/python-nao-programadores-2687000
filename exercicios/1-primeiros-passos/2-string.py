c0504180f6bf8ace43d71de5a779b43158f3f4ee
otacasoli <otaviocastroo@gmail.com>
2024-10-29 02:01:37 +0000

    ex2

--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -1,16 +1,16 @@
 resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
 
 # Imprima na tela a variável "resumo"
-
+print(resumo)
 
 # Imprima na tela apenas a segunda letra da variável
-
+print(resumo[1])
 
 # Imprima na tela a idade de Paloma (resposta esperada: "46")
-
+print(resumo[23])
 
 # Imprima na tela o trecho final da variável
-
+print(resumo[-1])
 
 # Converta todos as letras para minúsculo e imprima na tela
 
