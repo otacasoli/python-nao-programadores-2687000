43bd000c1da5f672c11bd6d84f02e55073c0dba9
otacasoli <otaviocastroo@gmail.com>
2024-10-29 02:01:28 +0000

    ex1.1

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,13 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Otavio'
+profissao = "Economista"
 
 # Declare variáveis de cada tipo numérico
-
+idade = 32
+altura = 1.72
 
 # Declare uma variável do tipo booleano
+Verdadeiro = True
+Falso = False
